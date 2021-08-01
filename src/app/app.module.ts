29a4ab3f9e0b9a2c8d88100c9de1b25ca1ce33f7
pebble-fish <pebble.fishie@gmail.com>
2021-08-01 16:27:56 -0400

    added themes, imported dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    AgGridModule.withComponents([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
